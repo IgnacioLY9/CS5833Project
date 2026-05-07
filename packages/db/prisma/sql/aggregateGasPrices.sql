189f26d9890579717700a8f12c36c94c8fd4c97a
Ignacio Yockers <ignacio.l.yockers-1@ou.edu>
2026-05-06 19:18:20 -0500

    added conflict detection to sql

--- packages/db/prisma/sql/aggregateGasPrices.sql
+++ packages/db/prisma/sql/aggregateGasPrices.sql
@@ -1,62 +1,103 @@
--- @name aggregateGasPrices
+-- @param {Int} $1:from The block number from which to start aggregating the data.
+-- @param {Int} $2:to The block number until which to aggregate the data.
+INSERT INTO gas_stats as curr_stats (
 
--- @param {Int} $1:from block number
--- @param {Int} $2:to block number
+  category,
+  rollup,
 
-WITH valid_txs AS (
-  SELECT
-    tx.*,
-    b.blob_gas_price,
-    b.number AS block_number
-  FROM transaction tx
-  JOIN block b
-    ON b.hash = tx.block_hash
-  LEFT JOIN transaction_fork tx_f
-    ON tx_f.block_hash = tx.block_hash
-   AND tx_f.hash = tx.hash
-  WHERE tx_f.hash IS NULL
-    AND b.number BETWEEN $1 AND $2
-),
-
-valid_blobs AS (
-  SELECT DISTINCT
-    bl.versioned_hash
-  FROM blob bl
-  JOIN blobs_on_transactions bl_txs
-    ON bl_txs.blob_hash = bl.versioned_hash
-  JOIN valid_txs tx
-    ON tx.hash = bl_txs.tx_hash
-),
-
-valid_blocks AS (
-  SELECT DISTINCT
-    b.number,
-    b.blob_gas_price
-  FROM block b
-  WHERE b.number BETWEEN $1 AND $2
-)
-
-INSERT INTO gas_stats (
-  avg_blob_gas_price,
-  max_blob_gas_price,
-  median_blob_gas_price,
-  min_blob_gas_price,
-  q1_blob_gas_price,
-  q3_blob_gas_price,
-  total_blob_gas_price,
   total_blobs,
   updated_at
 )
 SELECT
-  COALESCE(AVG(vb.blob_gas_price)::FLOAT, 0) AS avg_blob_gas_price,
-  COALESCE(MAX(vb.blob_gas_price)::FLOAT, 0) AS max_blob_gas_price,
-  COALESCE(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY vb.blob_gas_price), 0),
-  COALESCE(MIN(vb.blob_gas_price)::FLOAT, 0),
-  COALESCE(PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY vb.blob_gas_price), 0),
-  COALESCE(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY vb.blob_gas_price), 0),
+  CASE WHEN f.rollup IS NOT NULL THEN 'rollup'::category ELSE 'other'::category END AS category,
+  f.rollup,
 
-  COALESCE(SUM(vb.blob_gas_price)::DECIMAL, 0) AS total_blob_gas_price,
-  COALESCE(COUNT(vb.number)::INT, 0) AS total_blobs,
+  COALESCE(COUNT(bl_txs.blob_hash)::INT, 0) AS total_blobs,
 
-  NOW()
-FROM valid_blocks vb;
+  NOW() AS updated_at
+FROM blob bl
+  JOIN blobs_on_transactions bl_txs ON bl_txs.blob_hash = bl.versioned_hash
+  JOIN transaction tx ON tx.hash = bl_txs.tx_hash
+  JOIN address f ON f.address = tx.from_id
+  LEFT JOIN transaction_fork tx_f ON tx_f.block_hash = tx.block_hash AND tx_f.hash = tx.hash
+WHERE tx_f.hash IS NULL AND tx.block_number BETWEEN $1 AND $2
+GROUP BY GROUPING SETS (
+  (category),
+  (f.rollup),
+  ()
+)
+--  Exclude NULL rollup aggregates from the second grouping set, as they’re already included in the first when the category is OTHER
+HAVING NOT (
+  GROUPING(f.rollup) = 0 AND
+  f.rollup IS NULL
+)
+ON CONFLICT (category, rollup) DO UPDATE SET
+
+  total_blobs = curr_stats.total_blobs + EXCLUDED.total_blobs,
+
+  updated_at = EXCLUDED.updated_at
+
+;
+
+INSERT INTO overall_stats AS curr_stats (
+    category,
+    rollup,
+    avg_blob_gas_price,
+    min_blob_gas_price,
+    q1_blob_gas_price,
+    median_blob_gas_price,
+    q3_blob_gas_price,
+    max_blob_gas_price,
+    updated_at
+)
+SELECT
+    CASE
+        WHEN from_addr.rollup IS NOT NULL THEN 'rollup'::category
+        ELSE 'other'::category
+    END AS category,
+    from_addr.rollup,
+    COALESCE(AVG(b.blob_gas_price)::FLOAT, 0) AS avg_blob_gas_price,
+    COALESCE(MIN(b.blob_gas_price)::FLOAT, 0) AS min_blob_gas_price,
+    COALESCE(PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY b.blob_gas_price)::FLOAT, 0) AS q1_blob_gas_price,
+    COALESCE(PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY b.blob_gas_price)::FLOAT, 0) AS median_blob_gas_price,
+    COALESCE(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY b.blob_gas_price)::FLOAT, 0) AS q3_blob_gas_price,
+    COALESCE(MAX(b.blob_gas_price)::FLOAT, 0) AS max_blob_gas_price,
+    NOW() AS updated_at
+FROM transaction tx
+JOIN block b ON b.hash = tx.block_hash
+JOIN address from_addr ON from_addr.address = tx.from_id
+JOIN address to_addr ON to_addr.address = tx.to_id
+LEFT JOIN transaction_fork tx_f
+    ON tx_f.block_hash = b.hash
+   AND tx_f.hash = tx.hash
+WHERE tx_f.hash IS NULL
+  AND b.number BETWEEN $1 AND $2
+GROUP BY GROUPING SETS (
+    (category),
+    (from_addr.rollup),
+    ()
+)
+HAVING NOT (
+    GROUPING(from_addr.rollup) = 0
+    AND from_addr.rollup IS NULL
+)
+ON CONFLICT (category, rollup)
+DO UPDATE SET
+    total_transactions = curr_stats.total_transactions + EXCLUDED.total_transactions,
+    total_blob_gas_price = curr_stats.total_blob_gas_price + EXCLUDED.total_blob_gas_price,
+
+    avg_blob_gas_price =
+        CASE
+            WHEN curr_stats.total_transactions + EXCLUDED.total_transactions = 0 THEN 0
+            ELSE (curr_stats.total_blob_gas_price + EXCLUDED.total_blob_gas_price)
+                 / (curr_stats.total_transactions + EXCLUDED.total_transactions)
+        END,
+
+    min_blob_gas_price = LEAST(curr_stats.min_blob_gas_price, EXCLUDED.min_blob_gas_price),
+    max_blob_gas_price = GREATEST(curr_stats.max_blob_gas_price, EXCLUDED.max_blob_gas_price),
+
+    q1_blob_gas_price = EXCLUDED.q1_blob_gas_price,
+    median_blob_gas_price = EXCLUDED.median_blob_gas_price,
+    q3_blob_gas_price = EXCLUDED.q3_blob_gas_price,
+
+    updated_at = EXCLUDED.updated_at;
